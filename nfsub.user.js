412374128a4df8f3888867ed7dc238ed0841c225
Martin Algesten <martin@algesten.se>
2015-05-28 10:12:58 +0300

    tamper.user.js -> nfsub.user.js

--- nfsub.user.js
+++ nfsub.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Netflix Subtitles
 // @namespace    http://www.netflix.com
-// @version      1.0.9
+// @version      1.0.10
 // @description  subtitles on netflix
 // @author       Martin Algesten
 // @include      http://www.netflix.com/*
 // @grant        none
-// @downloadURL  https://raw.githubusercontent.com/algesten/nfsub/master/tamper.user.js
+// @downloadURL  https://raw.githubusercontent.com/algesten/nfsub/master/nfsub.user.js
 // @supportURL   https://github.com/algesten/nfsub/issues
 // ==/UserScript==
 
